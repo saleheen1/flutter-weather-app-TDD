bcc5a68c064f1f2c7756a45256c7b8ca2f98c29d
saleheen1 <smsaleheen3@gmail.com>
2024-09-27 12:25:34 +0600

    domain and data layer code done, also in core folder error classes and api urls added

---
+++ android/app/src/main/kotlin/com/example/flutter_weather_app_tdd/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_weather_app_tdd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/domain/entities/weather.dart
@@ -0,0 +1,19 @@
+class Weather {
+  const Weather({
+    required this.cityName,
+    required this.main,
+    required this.description,
+    required this.iconCode,
+    required this.temperature,
+    required this.pressure,
+    required this.humidity,
+  });
+
+  final String cityName;
+  final String main;
+  final String description;
+  final String iconCode;
+  final double temperature;
+  final int pressure;
+  final int humidity;
+}
